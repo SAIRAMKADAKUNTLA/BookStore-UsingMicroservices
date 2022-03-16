194ef82bef8d794cd7002eec074ca407e1e6fd85
Sairam Kadakuntla <83599427+SAIRAMKADAKUNTLA@users.noreply.github.com>
2022-03-16 09:12:09 +0530

    Added:implementing gateway&registry  microservices

---
+++ AllServices-Registry/src/test/java/com/bridgelabz/allservicesregistry/AllServicesRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.allservicesregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllServicesRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
